61bebaf28faa781e55ead90c1e4aa9009e24387c
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2024-02-01 16:20:47 +0800

    Added: bootstrap config

--- app/src/App.vue
+++ app/src/App.vue
@@ -13,7 +13,16 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <button 
+      class="btn btn-primary" 
+      data-bs-target="#collapseTarget" 
+      data-bs-toggle="collapse">
+      Bootstrap collapse
+    </button>
+    <div class="collapse py-2" id="collapseTarget">
+      This is the toggle-able content!
+    </div>
   </main>
 </template>
 
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import * as bootstrap from 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
